7769b73d4020a4190dce1cb3ebeb68bc35b9e781
PRATIK SALVE <pratikrunner@gmail.com>
2024-12-20 14:50:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Pratik!");
         System.out.println("HOW ARE YOU TODAY");
+        System.out.println("HOW ARE YOU TODAY!!");
     }
 }
